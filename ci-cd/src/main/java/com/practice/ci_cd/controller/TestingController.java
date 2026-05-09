35801354875e211ff0ebf0a92a7020ff2b2e19a8
murtaza838 <murtaza.khalid@paysyslabs.com>
2026-05-09 21:41:03 +0500

    testing1

---
+++ ci-cd/src/main/java/com/practice/ci_cd/controller/TestingController.java
@@ -0,0 +1,20 @@
+package com.practice.ci_cd.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/testing")
+public class TestingController {
+
+    @GetMapping("/testing")
+    public ResponseEntity<?> testingMyApi() throws Exception {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("message", "API is working fine");
+        return ResponseEntity.ok(response);
+    }
+}
